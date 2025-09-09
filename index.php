dd94b10d231ff8c0b6b0adbd2a263f8a7a446f05
Daniel Tran <danieltran354@gmail.com>
2025-09-09 12:53:55 +0200

    changed index.php to use database.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
-// Gegevens voor de connectie
-//$host       = '127.0.0.1';
-//$username   = 'root';
-//$password   = '';
-//$database   = 'cleanbotpro';
-//
-//$db = mysqli_connect($host, $username, $password, $database)
-//or die('Error: '.mysqli_connect_error());
+//include_once 'includes/database.php';
+
 //
 //$query = "SELECT * FROM tasks";
 //

92c624d18a63180b0f996920d45f5872e89fb43d
Daniel Tran <danieltran354@gmail.com>
2025-09-10 13:03:06 +0200

    delete.php and details.php finished

--- delete.php
+++ delete.php
@@ -38,7 +38,7 @@ mysqli_close($db);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Delete Album</title>
+    <title>Delete Task</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
 </head>
 <body>
